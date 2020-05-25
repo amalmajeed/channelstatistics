d240fb7e10b7d6cfd56aa535f0621c95729b8f2e
Amal Majeed <amalmajeed7@gmail.com>
2020-05-25 16:11:12 -0600

    Added code to request json and dump it to file

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 # Reading API KEY
-
 file = open("apikey.txt","r")
 API_KEY = file.read()[:-1]
 file.close()
+
+# channel id - PowerfulJRE
+channel_id = "UCzQUP1qoWDoEbmsQxvdjxgQ"
+
+from youtube_statistics import YTstats
+
+
+
+
+
+
+
+
+yt = YTstats(API_KEY,channel_id)
+data = yt.get_channel_statistics()
+yt.dump()
--- youtube_statistics.py
+++ youtube_statistics.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 
 class YTstats:
 
@@ -5,6 +8,32 @@ class YTstats:
 		self.api_key = api_key
 		self.channel_id = channel_id
 		self.channel_statistics = None
+		self.channel_title = ''
 
 	def get_channel_statistics(self):
-		url = f''
+		url = f'https://www.googleapis.com/youtube/v3/channels?part=statistics,snippet&id={self.channel_id}&key={self.api_key}'
+		#print(url)
+		json_url = requests.get(url)
+		#print("The response is :",json_url,"\n")
+		# Getting the actual text of the website
+		data = json.loads(json_url.text)
+		self.channel_title = data["items"][0]["snippet"]["title"]
+		#print("The data is : ",data,"\n")
+		try:
+			data = data["items"][0]['statistics']
+		except:
+			data = None
+		self.channel_statistics = data
+		return data
+
+	def dump(self):
+		if self.channel_statistics is None:
+			return
+
+		channel_title = self.channel_title 
+		channel_title = channel_title.replace(" ","_").lower()
+		file_name = channel_title + ".json"
+		with open(file_name,"w") as f:
+			json.dump(self.channel_statistics,f,indent = 4)
+		print("file dumped !\n")
+
